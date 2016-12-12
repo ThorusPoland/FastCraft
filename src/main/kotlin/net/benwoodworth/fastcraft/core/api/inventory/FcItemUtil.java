aa4e67ff77744254e65c5b87f8e6ce64f0a61d66
Ben Woodworth <ben@benwoodworth.net>
2016-12-12 14:01:36 -0500

    Rename FcItemComparator to FcItemUtil. Added hashCode() method to FcItemUtil.

--- src/main/kotlin/net/benwoodworth/fastcraft/core/api/inventory/FcItemUtil.java
+++ src/main/kotlin/net/benwoodworth/fastcraft/core/api/inventory/FcItemUtil.java
@@ -5,7 +5,7 @@ package net.benwoodworth.fastcraft.core.api.inventory;
  *
  * @param <TItem> The type of item being compared.
  */
-public abstract class FcItemComparator<TItem extends FcItem<?>> {
+public abstract class FcItemUtil<TItem extends FcItem<?>> {
 
     /**
      * Compares two items for equality.
@@ -14,7 +14,7 @@ public abstract class FcItemComparator<TItem extends FcItem<?>> {
      * @param item1 An item to compare.
      * @return Returns true if the items are equal.
      */
-    boolean areEqual(TItem item0, TItem item1) {
+    public boolean areEqual(TItem item0, TItem item1) {
         return item0.getAmount() == item1.getAmount()
                 && areSimilar(item0, item1);
     }
@@ -26,8 +26,7 @@ public abstract class FcItemComparator<TItem extends FcItem<?>> {
      * @param item1 An item to compare.
      * @return Returns true if the items are similar.
      */
-    abstract boolean areSimilar(TItem item0, TItem item1);
-
+    public abstract boolean areSimilar(TItem item0, TItem item1);
 
     /**
      * Checks to see if an item can be used as an ingredient.
@@ -36,5 +35,13 @@ public abstract class FcItemComparator<TItem extends FcItem<?>> {
      * @param ingredient The recipe ingredient.
      * @return Returns true if the item can be used as an ingredient.
      */
-    abstract boolean matchesIngredient(TItem item, TItem ingredient);
+    public abstract boolean matchesIngredient(TItem item, TItem ingredient);
+
+    /**
+     * Evaluate an item's hash code.
+     *
+     * @param item The item to hash.
+     * @return Returns the item's hash code.
+     */
+    public abstract int hashCode(TItem item);
 }
