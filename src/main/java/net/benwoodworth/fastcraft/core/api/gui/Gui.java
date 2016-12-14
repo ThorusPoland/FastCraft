ace476dc3bb054930676ed3cff09e5d60c183f25
Ben Woodworth <ben@benwoodworth.net>
2016-12-14 13:51:47 -0500

    Started creating GUI API.

--- src/main/java/net/benwoodworth/fastcraft/core/api/gui/Gui.java
+++ src/main/java/net/benwoodworth/fastcraft/core/api/gui/Gui.java
@@ -1,8 +1,34 @@
 package net.benwoodworth.fastcraft.core.api.gui;
 
+import net.benwoodworth.fastcraft.core.api.dependencies.inventory.FcInventory;
+
 /**
  * A user interface for
  * @param <TItem>
  */
-public class Gui<TItem> implements GuiLayout<TItem> {
+public class Gui<TItem> extends GuiLayoutAbstract<TItem> {
+
+    private GuiApi<TItem> guiApi;
+    private FcInventory<TItem> inventory;
+
+    /**
+     * Create a new GUI.
+     *
+     * @param guiApi The GUI API.
+     * @param inventory The inventory representing this GUI.
+     */
+    public Gui(GuiApi<TItem> guiApi, FcInventory<TItem> inventory) {
+        this.guiApi = guiApi;
+        this.inventory = inventory;
+    }
+
+    @Override
+    public int getWidth() {
+        return inventory.getItemGrid().getWidth();
+    }
+
+    @Override
+    public int getHeight() {
+        return inventory.getItemGrid().getHeight();
+    }
 }
--- src/main/java/net/benwoodworth/fastcraft/core/api/gui/GuiLayout.java
+++ src/main/java/net/benwoodworth/fastcraft/core/api/gui/GuiLayout.java
@@ -1,9 +1,48 @@
 package net.benwoodworth.fastcraft.core.api.gui;
 
+import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
- * Created by bwoodworth on 12/13/2016.
+ * The layout of a GUI.
+ * Layouts are composite, so they're made up of other layouts.
+ *
+ * @param <TItem> The native item type.
  */
-public interface GuiLayout<TItem> {
+public class GuiLayout<TItem> extends GuiLayoutAbstract {
 
+    private int width, height;
 
+    /**
+     * Create a new GuiLayout.
+     *
+     * @param width The width of the layout.
+     * @param height The height of the layout.
+     */
+    public GuiLayout(int width, int height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    /**
+     * Get the width of this layout.
+     *
+     * @return Returns the width of this layout.
+     */
+    @Override
+    public int getWidth() {
+        return width;
+    }
+
+    /**
+     * Get the height of this layout.
+     *
+     * @return Returns the height of this layout.
+     */
+    @Override
+    public int getHeight() {
+        return height;
+    }
 }
