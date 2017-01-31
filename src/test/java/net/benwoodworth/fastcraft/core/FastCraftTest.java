fdaf0ba057e5d7f3705f7e2874a0a2f281646e59
Ben Woodworth <ben@benwoodworth.net>
2017-01-31 13:00:09 -0500

    Remove intentionally failing test.

--- src/test/java/net/benwoodworth/fastcraft/core/FastCraftTest.java
+++ src/test/java/net/benwoodworth/fastcraft/core/FastCraftTest.java
@@ -7,11 +7,5 @@ import org.junit.Test;
  * Created by Ben on 1/31/2017.
  */
 public class FastCraftTest {
-    @Test
-    public void getDependencies() {
-        System.out.println("Test1");
-        assertTrue("Intentionally failing to test CI", false);
-        System.out.println("Test2");
-    }
 
 }
