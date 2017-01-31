9bb5d938b0d0bc96cf678525f0f78d01e30cf939
Ben Woodworth <ben@benwoodworth.net>
2017-01-31 12:16:43 -0500

    Added failing test to test CI

---
+++ src/test/java/net/benwoodworth/fastcraft/core/FastCraftTest.java
@@ -0,0 +1,15 @@
+package net.benwoodworth.fastcraft.core;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+/**
+ * Created by Ben on 1/31/2017.
+ */
+class FastCraftTest {
+    @Test
+    void getDependencies() {
+        assertTrue("Intentionally failing to test CI", false);
+    }
+
+}
