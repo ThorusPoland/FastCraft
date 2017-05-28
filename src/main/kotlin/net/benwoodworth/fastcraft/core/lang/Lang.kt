aed9a530129fc82a0cc611a4d4f0ef41e03c6053
BenWoodworth <ben@benwoodworth.net>
2017-05-27 21:47:07 -0400

    Added localization

---
+++ src/main/kotlin/net/benwoodworth/fastcraft/core/lang/Lang.kt
@@ -0,0 +1,66 @@
+package net.benwoodworth.fastcraft.core.lang
+
+/**
+ * FastCraft localization.
+ *
+ * If you provide a localization, feel free to PM Kepler_ on spigotmc.org
+ *
+ * Use the section symbol (§) for formatting/color codes:
+ * http://minecraft.gamepedia.com/Formatting_codes
+ */
+abstract class Lang(val localeId: String) {
+    companion object {
+        /**
+         * All the Lang translations.
+         */
+        val all = listOf(
+                LangEnUs()
+        )
+
+        /**
+         * Get a Lang given a locale ID.
+         */
+        fun getLang(localeId: String) = all.single {
+            it.localeId.toLowerCase() == localeId.toLowerCase()
+        }
+    }
+
+    abstract fun gui_title(): String
+
+    abstract fun gui_ingredients_label(): String
+    abstract fun gui_ingredients_item(item: String, amount: Int): String
+
+    abstract fun gui_results_label(): String
+    abstract fun gui_results_item(item: String, amount: Int): String
+
+    abstract fun gui_recipeHash_label(): String
+    abstract fun gui_recipeHash_code(hashcode: String): String
+
+    abstract fun gui_toolbar_prev_title(prev: Int, total: Int): String
+    abstract fun gui_toolbar_prev_desc(prev: Int, total: Int): List<String>
+
+    abstract fun gui_toolbar_next_title(next: Int, total: Int): String
+    abstract fun gui_toolbar_next_desc(next: Int, total: Int): List<String>
+
+    abstract fun gui_toolbar_refresh_title(): String
+    abstract fun gui_toolbar_refresh_desc(): List<String>
+
+    abstract fun gui_toolbar_multiplier_title(multiplier: Int): String
+    abstract fun gui_toolbar_multiplier_desc(multiplier: Int): List<String>
+
+    abstract fun gui_toolbar_workbench_title(): String
+    abstract fun gui_toolbar_workbench_desc(): List<String>
+
+    abstract fun command_err_usage(commandUsage: String): String
+    abstract fun command_err_noPerm(permission: String): String
+    abstract fun command_err_playerOnly(): String
+    abstract fun command_err_consoleOnly(): String
+    abstract fun command_err_unknownPlayer(player: String): String
+
+    abstract fun command_fc_toggle_output_self_on(): String
+    abstract fun command_fc_toggle_output_self_off(): String
+    abstract fun command_fc_toggle_output_other_on(player: String): String
+    abstract fun command_fc_toggle_output_other_off(player: String): String
+
+    abstract fun command_fca_reload(): String
+}
---
+++ src/main/kotlin/net/benwoodworth/fastcraft/core/lang/LangEnUs.kt
@@ -0,0 +1,59 @@
+package net.benwoodworth.fastcraft.core.lang
+
+/**
+ * Created by ben on 5/27/17.
+ */
+class LangEnUs : Lang("EN-US") {
+
+    override fun gui_title() = "FastCraft"
+
+    override fun gui_ingredients_label() = "§aIngredients"
+    override fun gui_ingredients_item(item: String, amount: Int) = "§a ${amount}x $item"
+
+    override fun gui_results_label() = "§aResults:"
+    override fun gui_results_item(item: String, amount: Int) = "§a ${amount}x $item"
+
+    override fun gui_recipeHash_label() = "§aHashCode:"
+    override fun gui_recipeHash_code(hashcode: String) = "§7 $hashcode"
+
+    override fun gui_toolbar_prev_title(prev: Int, total: Int) = "§aPrevious Page"
+    override fun gui_toolbar_prev_desc(prev: Int, total: Int) = listOf(
+            "§bGo to page $prev/$total"
+    )
+
+    override fun gui_toolbar_next_title(next: Int, total: Int) = "§aNext Page"
+    override fun gui_toolbar_next_desc(next: Int, total: Int) = listOf(
+            "§bGo to page $next/$total"
+    )
+
+    override fun gui_toolbar_refresh_title() = "§aRefresh"
+    override fun gui_toolbar_refresh_desc() = listOf(
+            "§bRefresh the FastCraft interface"
+    )
+
+    override fun gui_toolbar_multiplier_title(multiplier: Int) = "§aCrafting Multiplier (${multiplier}x)"
+    override fun gui_toolbar_multiplier_desc(multiplier: Int) = listOf(
+            "§bLeft/right click to increase/decrease",
+            "§bShift click to adjust by 1",
+            "§bMiddle click to reset to 1x"
+    )
+
+    override fun gui_toolbar_workbench_title() = "§aCrafting Grid"
+    override fun gui_toolbar_workbench_desc() = listOf(
+            "§bOpen a 3x3 crafting grid",
+            "§bUse §a/fc toggle §bto disable FastCraft"
+    )
+
+    override fun command_err_usage(commandUsage: String) = "§cCommand usage: $commandUsage"
+    override fun command_err_noPerm(permission: String) = "§cYou don't have the required permission for this command: $permission"
+    override fun command_err_playerOnly() = "§cYou must be a player to use this command."
+    override fun command_err_consoleOnly() = "§cThis command can only be executed in the server console"
+    override fun command_err_unknownPlayer(player: String) = "§cUnknown player: $player"
+
+    override fun command_fc_toggle_output_self_on() = "§aEnabled FastCraft+ interface."
+    override fun command_fc_toggle_output_self_off() = "§aDisabled FastCraft+ interface."
+    override fun command_fc_toggle_output_other_on(player: String) = "§aEnabled FastCraft+ interface for $player."
+    override fun command_fc_toggle_output_other_off(player: String) = "§aDisabled FastCraft+ interface for $player."
+
+    override fun command_fca_reload() = "§aFastCraft+ config files reloaded."
+}
