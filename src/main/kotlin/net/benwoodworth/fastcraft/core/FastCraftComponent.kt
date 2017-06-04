157a371b6854ec5181f9490c98a856a451a977ac
BuildTools <unconfigured@null.spigotmc.org>
2017-06-04 00:34:29 -0400

    Optimized imports.

--- src/main/kotlin/net/benwoodworth/fastcraft/core/FastCraftComponent.kt
+++ src/main/kotlin/net/benwoodworth/fastcraft/core/FastCraftComponent.kt
@@ -1,9 +1,7 @@
 package net.benwoodworth.fastcraft.core
 
-import dagger.Provides
 import net.benwoodworth.fastcraft.core.dependencies.event.EventListenerRegistry
 import net.benwoodworth.fastcraft.core.dependencies.inventory.FcItem
-import javax.inject.Singleton
 
 /**
  * A Dagger component for injecting FastCraft dependencies.
