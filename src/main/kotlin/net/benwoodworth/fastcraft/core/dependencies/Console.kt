a50ff42993922250b02855e9e7870b2f17c12242
BuildTools <ben@benwoodworth.net>
2017-06-17 21:19:43 -0400

    Rename ConsoleLogger to Console.

--- src/main/kotlin/net/benwoodworth/fastcraft/core/dependencies/Console.kt
+++ src/main/kotlin/net/benwoodworth/fastcraft/core/dependencies/Console.kt
@@ -5,33 +5,33 @@ import net.benwoodworth.fastcraft.core.dependencies.text.Text
 /**
  * Logs messages to the server console.
  */
-interface ConsoleLogger {
+interface Console {
 
     /**
      * Log an error to the console.
      *
      * @param message the error message to log
      */
-    fun error(message: Text)
+    fun logError(message: Text)
 
     /**
      * Log info to the console.
      *
      * @param message the info to log
      */
-    fun info(message: Text)
+    fun logInfo(message: Text)
 
     /**
      * Log a warning to the console.
      *
      * @param message the warning to log
      */
-    fun warn(message: Text)
+    fun logWarning(message: Text)
 
     /**
      * Log a debug message to the console, only if debug output is enabled.
      *
      * @param message the debug message to log
      */
-    fun debug(message: Text)
+    fun logDebug(message: Text)
 }
