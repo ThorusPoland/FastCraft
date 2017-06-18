c1e5473530eb35b77acfeb35cbee9bbc6cd29a57
BuildTools <ben@benwoodworth.net>
2017-06-17 21:18:45 -0400

    Use Text in ConsoleLogger

--- src/main/kotlin/net/benwoodworth/fastcraft/core/dependencies/ConsoleLogger.kt
+++ src/main/kotlin/net/benwoodworth/fastcraft/core/dependencies/ConsoleLogger.kt
@@ -1,5 +1,7 @@
 package net.benwoodworth.fastcraft.core.dependencies
 
+import net.benwoodworth.fastcraft.core.dependencies.text.Text
+
 /**
  * Logs messages to the server console.
  */
@@ -10,26 +12,26 @@ interface ConsoleLogger {
      *
      * @param message the error message to log
      */
-    fun error(message: String)
+    fun error(message: Text)
 
     /**
      * Log info to the console.
      *
      * @param message the info to log
      */
-    fun info(message: String)
+    fun info(message: Text)
 
     /**
      * Log a warning to the console.
      *
      * @param message the warning to log
      */
-    fun warn(message: String)
+    fun warn(message: Text)
 
     /**
      * Log a debug message to the console, only if debug output is enabled.
      *
      * @param message the debug message to log
      */
-    fun debug(message: String)
+    fun debug(message: Text)
 }
