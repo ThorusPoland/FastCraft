def42f877e9d48befb6eee6c87352ecaab12186e
BuildTools <ben@benwoodworth.net>
2017-06-26 22:31:15 -0400

    Add FastCraft dependencies.

---
+++ src/main/kotlin/net/benwoodworth/fastcraft/core/dependencies/fastcraft/ModuleFastCraft.kt
@@ -0,0 +1,9 @@
+package net.benwoodworth.fastcraft.core.dependencies.fastcraft
+
+/**
+ * Created by ben on 6/26/17.
+ */
+interface ModuleFastCraft {
+
+    fun pluginFastCraft(): PluginFastCraft
+}
---
+++ src/main/kotlin/net/benwoodworth/fastcraft/core/dependencies/fastcraft/PluginFastCraft.kt
@@ -0,0 +1,8 @@
+package net.benwoodworth.fastcraft.core.dependencies.fastcraft
+
+import net.benwoodworth.fastcraft.core.dependencies.server.Plugin
+
+/**
+ * The FastCraft plugin.
+ */
+interface PluginFastCraft : Plugin
