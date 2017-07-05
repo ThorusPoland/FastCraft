fc7649fc1711eb5e0da83be93d5a166a435af8de
BuildTools <ben@benwoodworth.net>
2017-07-05 01:43:15 -0400

    Implemented SpongeConfigSectionAdapter.

--- src/main/kotlin/net/benwoodworth/fastcraft/sponge/dependencies/config/SpongeConfigSectionAdapter.kt
+++ src/main/kotlin/net/benwoodworth/fastcraft/sponge/dependencies/config/SpongeConfigSectionAdapter.kt
@@ -16,7 +16,11 @@ class SpongeConfigSectionAdapter(
         return SpongeConfigSectionAdapter(base.getNode(key))
     }
 
-    override fun remove(key: String) {
+    override fun removeSection(key: String) {
+        base.removeChild(key)
+    }
+
+    override fun removeValue(key: String) {
         base.removeChild(key)
     }
 
@@ -26,5 +30,21 @@ class SpongeConfigSectionAdapter(
 
     override fun setString(key: String, value: String) {
         base.getNode(key).setValue(TypeTokens.STRING_TOKEN, value)
+    }
+
+    override fun getInt(key: String): Int? {
+        return base.getNode(key).int
+    }
+
+    override fun setInt(key: String, value: Int) {
+        base.getNode(key).setValue(TypeTokens.INTEGER_TOKEN, value)
+    }
+
+    override fun getBoolean(key: String): Boolean? {
+        return base.getNode(key).boolean
+    }
+
+    override fun setBoolean(key: String, value: Boolean) {
+        base.getNode(key).setValue(TypeTokens.BOOLEAN_TOKEN, value)
     }
 }
