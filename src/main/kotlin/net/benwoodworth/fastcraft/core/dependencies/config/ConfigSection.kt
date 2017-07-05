dc84c33d7bacc56a59e00f832154d344e9231712
BuildTools <ben@benwoodworth.net>
2017-07-05 01:38:07 -0400

    Change ConfigSection interface.

--- src/main/kotlin/net/benwoodworth/fastcraft/core/dependencies/config/ConfigSection.kt
+++ src/main/kotlin/net/benwoodworth/fastcraft/core/dependencies/config/ConfigSection.kt
@@ -22,11 +22,18 @@ interface ConfigSection {
     operator fun get(key: String) = getSection(key)
 
     /**
-     * Remove a value or config section at the given key.
+     * Remove the section at the given key.
      *
      * @param key the key to remove
      */
-    fun remove(key: String)
+    fun removeSection(key: String)
+
+    /**
+     * Remove the value at the given key.
+     *
+     * @param key the key to remove
+     */
+    fun removeValue(key: String)
 
     /**
      * Get a [String] at the given key.
@@ -43,4 +50,36 @@ interface ConfigSection {
      * @param value the value to set
      */
     fun setString(key: String, value: String)
+
+    /**
+     * Get an [Int] at the given key.
+     *
+     * @param key the key
+     * @return the value at the given key, or null if the key does not exist
+     */
+    fun getInt(key: String): Int?
+
+    /**
+     * Set a [String] at a given key.
+     *
+     * @param key the key
+     * @param value the value to set
+     */
+    fun setInt(key: String, value: Int)
+
+    /**
+     * Get a [Boolean] at the given key.
+     *
+     * @param key the key
+     * @return the value at the given key, or null if the key does not exist
+     */
+    fun getBoolean(key: String): Boolean?
+
+    /**
+     * Set a [Boolean] at a given key.
+     *
+     * @param key the key
+     * @param value the value to set
+     */
+    fun setBoolean(key: String, value: Boolean)
 }
