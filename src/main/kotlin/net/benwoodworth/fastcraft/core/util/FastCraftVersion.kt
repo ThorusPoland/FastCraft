a477d3c99f7c24536336bf1f8e5abab501f17a2e
ben@benwoodworth.net <ben@benwoodworth.net>
2017-08-02 20:08:10 -0400

    Move FastCraftVersion into core.util. Make changes to FastCraftVersion implementation.

--- src/main/kotlin/net/benwoodworth/fastcraft/core/util/FastCraftVersion.kt
+++ src/main/kotlin/net/benwoodworth/fastcraft/core/util/FastCraftVersion.kt
@@ -3,30 +3,48 @@ package net.benwoodworth.fastcraft.core.util
 /**
  * A FastCraft version.
  */
-class FastCraftVersion(
-        val major: Int,
-        val minor: Int,
-        val patch: Int = 0,
-        val label: String? = null,
-        val build: Int? = null
-) : Comparable<FastCraftVersion> {
+class FastCraftVersion : Comparable<FastCraftVersion> {
 
-    companion object {
+    val major: Int
+    val minor: Int
+    val patch: Int
+    val label: String?
+    val build: Int?
 
-        fun parse(version: String): FastCraftVersion {
-            val regex = Regex("^(\\d+)\\.(\\d+)(?:\\.(\\d+))?(?:-([a-zA-Z]+))?(?:-(\\d+))?\$")
+    /**
+     * Construct a FastCraftVersion from its version parts.
+     */
+    constructor(
+            major: Int,
+            minor: Int,
+            patch: Int = 0,
+            label: String? = null,
+            build: Int? = null
+    ) {
+        this.major = major
+        this.minor = minor
+        this.patch = patch
+        this.label = label
+        this.build = build
+    }
 
-            val matches = regex.find(version) ?: throw IllegalArgumentException("Invalid FastCraft version: $version")
+    /**
+     * Construct a [FastCraftVersion] given a version string.
+     *
+     * @param version the version string
+     */
+    constructor(version: String) {
+        val regex = Regex("^(\\d+)\\.(\\d+)(?:\\.(\\d+))?(?:-([a-zA-Z]+))?(?:-(\\d+))?\$")
 
-            val (major, minor, patch, label, build) = matches.destructured
-            return FastCraftVersion(
-                    major.toInt(),
-                    minor.toInt(),
-                    patch.takeIf { it.isNotEmpty() }?.toInt() ?: 0,
-                    label.takeIf { it.isNotEmpty() },
-                    build.takeIf { it.isNotEmpty() }?.toInt()
-            )
-        }
+        val matches = regex.find(version) ?: throw IllegalArgumentException("Invalid FastCraft version: $version")
+
+        val (major, minor, patch, label, build) = matches.destructured
+
+        this.major = major.toInt()
+        this.minor = minor.toInt()
+        this.patch = patch.takeIf { it.isNotEmpty() }?.toInt() ?: 0
+        this.label = label.takeIf { it.isNotEmpty() }
+        this.build = build.takeIf { it.isNotEmpty() }?.toInt()
     }
 
     override fun toString(): String {
@@ -55,5 +73,24 @@ class FastCraftVersion(
             build != null && other.build != null -> build - other.build
             else -> 0
         }
+    }
+
+    override fun equals(other: Any?): Boolean {
+        return other is FastCraftVersion
+                && major == other.major
+                && minor == other.minor
+                && patch == other.patch
+                && label == other.label
+                && build == other.build
+    }
+
+    override fun hashCode(): Int {
+        var result = 0
+        result = result * 31 + major
+        result = result * 31 + minor
+        result = result * 31 + patch
+        result = result * 31 + (label?.hashCode() ?: 0)
+        result = result * 31 + (build?.hashCode() ?: 0)
+        return result
     }
 }
