3ccb61383fed3d8ece0fec575c4c46b2412168f6
BenWoodworth <ben@benwoodworth.net>
2018-01-01 21:55:03 -0500

    Re-add removed test files

---
+++ src/test/kotlin/net/benwoodworth/fastcraft/implementations/bukkit/config/BukkitConfigTests.kt
@@ -0,0 +1,20 @@
+package net.benwoodworth.fastcraft.implementations.bukkit.config
+
+import net.benwoodworth.fastcraft.dependencies.abstractions.config.ConfigTests
+import net.benwoodworth.fastcraft.dependencies.config.Config
+import org.bukkit.configuration.file.YamlConfiguration
+import java.nio.file.Path
+import java.nio.file.Paths
+
+/**
+ * Tests for [BukkitConfig].
+ */
+class BukkitConfigTests : ConfigTests() {
+
+    override val configFile: Path
+        get() = Paths.get(javaClass.classLoader.getResource("config-bukkit.yml").toURI())
+
+    override fun createInstance(): Config {
+        return BukkitConfig(YamlConfiguration())
+    }
+}
