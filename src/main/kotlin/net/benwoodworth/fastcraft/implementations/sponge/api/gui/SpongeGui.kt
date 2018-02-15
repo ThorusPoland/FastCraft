27dc9f13178f5b3408c850016a69376088cd8b88
BenWoodworth <ben@benwoodworth.net>
2018-02-15 18:50:42 -0500

    Worked on SpongeGui impementation

--- src/main/kotlin/net/benwoodworth/fastcraft/implementations/sponge/api/gui/SpongeGui.kt
+++ src/main/kotlin/net/benwoodworth/fastcraft/implementations/sponge/api/gui/SpongeGui.kt
@@ -2,20 +2,26 @@ package net.benwoodworth.fastcraft.implementations.sponge.api.gui
 
 import net.benwoodworth.fastcraft.api.gui.Gui
 import net.benwoodworth.fastcraft.api.gui.GuiAbstract
+import net.benwoodworth.fastcraft.api.gui.layout.GuiLayout
 import net.benwoodworth.fastcraft.dependencies.player.Player
+import net.benwoodworth.fastcraft.implementations.sponge.SpongeFastCraft
 import net.benwoodworth.fastcraft.implementations.sponge.item.SpongeItem
 import net.benwoodworth.fastcraft.implementations.sponge.player.SpongePlayer
 import net.benwoodworth.fastcraft.implementations.sponge.text.SpongeText
 import org.spongepowered.api.Sponge
 import org.spongepowered.api.item.inventory.Carrier
+import org.spongepowered.api.item.inventory.Inventory
+import org.spongepowered.api.item.inventory.ItemStack
 import org.spongepowered.api.item.inventory.property.InventoryTitle
 import org.spongepowered.api.item.inventory.type.CarriedInventory
+import org.spongepowered.api.item.inventory.type.GridInventory
+import org.spongepowered.api.item.inventory.type.Inventory2D
 import org.spongepowered.api.text.Text as Sponge_Text
 
 /**
  * Sponge implementation of [Gui].
  */
-abstract class SpongeGui<TInv : CarriedInventory<SpongeGui<TInv>>>(
+abstract class SpongeGui<out TInv : Inventory>(
         plugin: Any,
         invProvider: (SpongeGui<TInv>) -> TInv
 ) : GuiAbstract(), Carrier {
@@ -32,7 +38,9 @@ abstract class SpongeGui<TInv : CarriedInventory<SpongeGui<TInv>>>(
     }
 
     @Suppress("LeakingThis")
-    private val inventory = invProvider(this)
+    private val inventory: TInv = invProvider(this).also {
+        require(it is CarriedInventory<*>)
+    }
 
     override fun getInventory() = inventory
 
@@ -52,15 +60,36 @@ abstract class SpongeGui<TInv : CarriedInventory<SpongeGui<TInv>>>(
                 .map(::SpongePlayer)
     }
 
-    protected abstract fun getLayoutPos(slot: Int): LayoutPos?
+    protected fun GuiLayout.getSpongeItem(x: Int, y: Int): ItemStack? {
+        return (getItem(x, y)?.mutableCopy() as SpongeItem.Mutable?)?.base
+    }
 
-    @Suppress("NULLABILITY_MISMATCH_BASED_ON_JAVA_ANNOTATIONS")
-    override fun updateLayout() {
-        for (x in 0 until width) {
-            for (y in 0 until height) {
-                val item = getButton(x, y)?.item?.mutableCopy() as SpongeItem.Mutable?
-                gridInventory.set(x, y, item?.base)
+    class Chest(
+            plugin: SpongeFastCraft,
+            invProvider: (SpongeGui<GridInventory>) -> GridInventory
+    ) : Gui.Chest, SpongeGui<GridInventory>(plugin, invProvider) {
+        override val layout = addLayout(9, inventory.columns)
+        override fun updateLayout() {
+            for (x in 0 until inventory.columns) {
+                for (y in 0 until inventory.rows) {
+                    @Suppress("NULLABILITY_MISMATCH_BASED_ON_JAVA_ANNOTATIONS")
+                    inventory.set(x, y, layout.getSpongeItem(x, y))
+                }
             }
         }
+    }
+
+    class Dispenser(
+            plugin: SpongeFastCraft,
+            invProvider: (SpongeGui) -> CarriedInventory<SpongeGui>
+    ) : Gui.Dispenser, SpongeGui(plugin, invProvider) {
+        override val layout = addLayout(3, 3)
+    }
+
+    class Hopper(
+            plugin: SpongeFastCraft,
+            invProvider: (SpongeGui) -> CarriedInventory<SpongeGui>
+    ) : Gui.Hopper, SpongeGui(plugin, invProvider) {
+        override val layout = addLayout(5, 1)
     }
 }
