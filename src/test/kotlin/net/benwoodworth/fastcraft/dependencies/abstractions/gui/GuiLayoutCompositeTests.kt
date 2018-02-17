92201f8c8d29f3054518439a9395a02fdb133827
BenWoodworth <ben@benwoodworth.net>
2018-02-17 12:08:15 -0500

    Removed tests.

--- src/test/kotlin/net/benwoodworth/fastcraft/dependencies/abstractions/gui/GuiLayoutCompositeTests.kt
+++
@@ -1,9 +0,0 @@
-package net.benwoodworth.fastcraft.dependencies.abstractions.gui
-
-/**
- * Tests for [GuiLayoutComposite].
- */
-class GuiLayoutCompositeTests : GuiLayoutTests() {
-
-    override fun createInstance() = GuiLayoutComposite(2, 2)
-}
