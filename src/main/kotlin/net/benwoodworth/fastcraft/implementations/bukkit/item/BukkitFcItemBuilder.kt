2824b2f0314bddee9807688f785390fea47f6cce
BenWoodworth <ben@benwoodworth.net>
2018-03-18 18:33:54 -0400

    Fixed BukkitFcItemBuilder implementation

--- src/main/kotlin/net/benwoodworth/fastcraft/implementations/bukkit/item/BukkitFcItemBuilder.kt
+++ src/main/kotlin/net/benwoodworth/fastcraft/implementations/bukkit/item/BukkitFcItemBuilder.kt
@@ -4,16 +4,19 @@ import net.benwoodworth.fastcraft.dependencies.item.FcItem
 import net.benwoodworth.fastcraft.dependencies.item.FcItemBuilder
 import net.benwoodworth.fastcraft.dependencies.item.FcItemType
 import net.benwoodworth.fastcraft.dependencies.text.FcText
+import net.benwoodworth.fastcraft.implementations.bukkit.text.BukkitFcText
 import org.bukkit.Material
 import org.bukkit.inventory.ItemStack
 import javax.inject.Inject
 
-open class BukkitFcItemBuilder @Inject constructor(
+class BukkitFcItemBuilder @Inject constructor(
+        private val itemFactory: BukkitFcItemFactory,
+        private val textFactory: BukkitFcText.Factory
 ) : FcItemBuilder {
 
     private var result = ItemStack(Material.AIR)
 
-    override fun build() = BukkitFcItem(result.clone())
+    override fun build(): FcItem = itemFactory.create(result.clone())
 
     override fun from(item: FcItem) = also {
         result = (item.mutableCopy() as BukkitFcItem.Mutable).base
@@ -28,15 +31,11 @@ open class BukkitFcItemBuilder @Inject constructor(
     }
 
     override fun displayName(displayName: FcText?) = also {
-        result.itemMeta = result.itemMeta.apply {
-            setDisplayName((displayName as BukkitFcText?)?.text)
-        }
+        textFactory.setItemDisplayName(result, displayName)
     }
 
     override fun lore(lore: List<FcText?>) = also {
-        result.itemMeta = result.itemMeta.apply {
-            this.lore = lore.map { (it as BukkitFcText?)?.text }
-        }
+        textFactory.setItemLore(result, lore)
     }
 
     override fun durability(durability: Int) = also {
