71e6c444d575a746d47d5aeeb4f3b6a74c9b6546
Ben Woodworth <ben@benwoodworth.net>
2018-09-26 00:09:17 -0400

    Created FastCraftPlatformModule.

--- fastcraft-plugin/src/main/kotlin/net/benwoodworth/fastcraft/FastCraftImplementation.kt
+++
@@ -1,9 +0,0 @@
-package net.benwoodworth.fastcraft
-
-/**
- * An implementation of FastCraft.
- */
-interface FastCraftImplementation {
-
-    val instance: net.benwoodworth.fastcraft.FastCraft
-}
---
+++ fastcraft-plugin/src/main/kotlin/net/benwoodworth/fastcraft/FastCraftPlatformModule.kt
@@ -0,0 +1,40 @@
+package net.benwoodworth.fastcraft
+
+import dagger.Module
+import dagger.Provides
+import net.benwoodworth.fastcraft.platform.api.PlatformModule
+import javax.inject.Singleton
+
+@Module
+class FastCraftPlatformModule(
+        private var module: PlatformModule
+) : PlatformModule {
+
+    @Provides
+    override fun createConfigBuilder() = module.createConfigBuilder()
+
+    @Provides
+    override fun createGuiBuilder() = module.createGuiBuilder()
+
+    @Provides
+    override fun createItemBuilder() = module.createItemBuilder()
+
+    @Provides
+    @Singleton
+    override fun getItemTypeFactory() = module.getItemTypeFactory()
+
+    @Provides
+    @Singleton
+    override fun getPlayerProvider() = module.getPlayerProvider()
+
+    @Provides
+    @Singleton
+    override fun getRecipeProvider() = module.getRecipeProvider()
+
+    @Provides
+    @Singleton
+    override fun getPluginProvider() = module.getPluginProvider()
+
+    @Provides
+    override fun createTaskBuilder() = module.createTaskBuilder()
+}
