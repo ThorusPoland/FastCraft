ed66f98bc939caa0c20694afb453ad035fa19749
Ben Woodworth <ben@benwoodworth.net>
2018-09-30 00:14:26 -0400

    Implemented methods in BukkitPlatformModule$1_13_R0_1.

--- fastcraft-platform/bukkit/bukkit-1.13-R0.1/src/main/kotlin/net/benwoodworth/fastcraft/platform/bukkit/BukkitPlatformModule$1_13_R0_1.kt
+++ fastcraft-platform/bukkit/bukkit-1.13-R0.1/src/main/kotlin/net/benwoodworth/fastcraft/platform/bukkit/BukkitPlatformModule$1_13_R0_1.kt
@@ -1,4 +1,64 @@
 package net.benwoodworth.fastcraft.platform.bukkit
 
+import net.benwoodworth.fastcraft.platform.api.config.FcConfigBuilder
+import net.benwoodworth.fastcraft.platform.api.gui.FcGuiBuilder
+import net.benwoodworth.fastcraft.platform.api.item.FcItemBuilder
+import net.benwoodworth.fastcraft.platform.api.item.FcItemTypes
+import net.benwoodworth.fastcraft.platform.api.player.FcPlayerProvider
+import net.benwoodworth.fastcraft.platform.api.recipe.FcRecipeService
+import net.benwoodworth.fastcraft.platform.api.server.FcLogger
+import net.benwoodworth.fastcraft.platform.api.server.FcTaskBuilder
+import net.benwoodworth.fastcraft.platform.bukkit.server.`BukkitFcLogger$1_13_R0_1`
+import net.benwoodworth.fastcraft.platform.bukkit.server.`BukkitFcTaskBuilder$1_13_R0_1`
+import org.bukkit.plugin.Plugin
+import java.nio.file.Path
+
 @Suppress("ClassName")
-class `BukkitPlatformModule$1_13_R0_1` : BukkitPlatformModule
+class `BukkitPlatformModule$1_13_R0_1`(
+    private val plugin: Plugin
+) : BukkitPlatformModule {
+
+    private val logger by lazy {
+        `BukkitFcLogger$1_13_R0_1`(plugin.logger)
+    }
+
+    override fun getLogger(): FcLogger {
+        return logger
+    }
+
+    override fun getConfigPath(): Path {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getPluginDirectory(): Path {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun createConfigBuilder(): FcConfigBuilder {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun createGuiBuilder(): FcGuiBuilder {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun createItemBuilder(): FcItemBuilder {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getItemTypes(): FcItemTypes {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getPlayerProvider(): FcPlayerProvider {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getRecipeService(): FcRecipeService {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun createTaskBuilder(): FcTaskBuilder {
+        return `BukkitFcTaskBuilder$1_13_R0_1`(plugin)
+    }
+}
