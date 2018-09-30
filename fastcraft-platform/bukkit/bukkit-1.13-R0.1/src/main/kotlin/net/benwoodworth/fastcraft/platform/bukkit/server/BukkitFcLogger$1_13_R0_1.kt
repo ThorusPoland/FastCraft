b744ffc21d98a7f0b8477537a1a043cbb1958a8f
Ben Woodworth <ben@benwoodworth.net>
2018-09-30 00:07:38 -0400

    Implemented BukkitFcLogger$1_13_R0_1

--- fastcraft-platform/bukkit/bukkit-1.13-R0.1/src/main/kotlin/net/benwoodworth/fastcraft/platform/bukkit/server/BukkitFcLogger$1_13_R0_1.kt
+++ fastcraft-platform/bukkit/bukkit-1.13-R0.1/src/main/kotlin/net/benwoodworth/fastcraft/platform/bukkit/server/BukkitFcLogger$1_13_R0_1.kt
@@ -1,4 +1,22 @@
 package net.benwoodworth.fastcraft.platform.bukkit.server
 
+import java.util.logging.Level
+import java.util.logging.Logger
+
 @Suppress("ClassName")
-class `BukkitFcLogger$1_13_R0_1` : BukkitFcLogger
+class `BukkitFcLogger$1_13_R0_1`(
+    private val logger: Logger
+) : BukkitFcLogger {
+
+    override fun error(error: String) {
+        logger.log(Level.SEVERE, error)
+    }
+
+    override fun info(info: String) {
+        logger.log(Level.INFO, info)
+    }
+
+    override fun warn(warning: String) {
+        logger.log(Level.WARNING, warning)
+    }
+}
