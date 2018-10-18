00d93d1ff6f79d2630cae40e9ba7ecd055f6d93a
ben <ben@benwoodworth.net>
2018-10-18 03:55:37 -0400

    Removed bukkit-1.7.2-R0.3 project

--- fastcraft-bukkit/bukkit-1.7.2-R0.3/src/main/kotlin/net/benwoodworth/fastcraft/platform/bukkit/item/BukkitFcItemType.kt
+++
@@ -1,9 +0,0 @@
-package net.benwoodworth.fastcraft.platform.bukkit.item
-
-import net.benwoodworth.fastcraft.platform.api.item.FcItemType
-import net.benwoodworth.fastcraft.util.Adapter
-import org.bukkit.Material
-
-class BukkitFcItemType(
-    override val base: Material
-) : FcItemType, Adapter<Material>()
