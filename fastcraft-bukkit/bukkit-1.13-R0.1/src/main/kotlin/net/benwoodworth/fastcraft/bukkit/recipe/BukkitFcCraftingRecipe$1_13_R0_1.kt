2c4e4a5b84b72991d596ce2571c92005d0a0bd67
ben <ben@benwoodworth.net>
2018-10-26 20:18:27 -0400

    Implemented recipe platform interfaces

--- fastcraft-bukkit/bukkit-1.13-R0.1/src/main/kotlin/net/benwoodworth/fastcraft/bukkit/recipe/BukkitFcCraftingRecipe$1_13_R0_1.kt
+++ fastcraft-bukkit/bukkit-1.13-R0.1/src/main/kotlin/net/benwoodworth/fastcraft/bukkit/recipe/BukkitFcCraftingRecipe$1_13_R0_1.kt
@@ -1,5 +1,12 @@
 package net.benwoodworth.fastcraft.bukkit.recipe
 
+import net.benwoodworth.fastcraft.platform.recipe.FcIngredient
+
 @Suppress("ClassName")
-class `BukkitFcCraftingRecipe$1_13_R0_1` : net.benwoodworth.fastcraft.bukkit.recipe.BukkitFcCraftingRecipe
+class `BukkitFcCraftingRecipe$1_13_R0_1` : net.benwoodworth.fastcraft.bukkit.recipe.BukkitFcCraftingRecipe {
+    override val id: String
+        get() = TODO("not implemented") //To change initializer of created properties use File | Settings | File Templates.
+    override val ingredients: List<FcIngredient>
+        get() = TODO("not implemented") //To change initializer of created properties use File | Settings | File Templates.
+}
 
--- fastcraft-bukkit/bukkit-1.13-R0.1/src/main/kotlin/net/benwoodworth/fastcraft/bukkit/recipe/BukkitFcIngredient$1_13_R0_1.kt
+++ fastcraft-bukkit/bukkit-1.13-R0.1/src/main/kotlin/net/benwoodworth/fastcraft/bukkit/recipe/BukkitFcIngredient$1_13_R0_1.kt
@@ -1,4 +1,10 @@
 package net.benwoodworth.fastcraft.bukkit.recipe
 
+import net.benwoodworth.fastcraft.platform.item.FcItem
+
 @Suppress("ClassName")
-class `BukkitFcIngredient$1_13_R0_1` : net.benwoodworth.fastcraft.bukkit.recipe.BukkitFcIngredient
+class `BukkitFcIngredient$1_13_R0_1` : net.benwoodworth.fastcraft.bukkit.recipe.BukkitFcIngredient {
+    override fun matches(item: FcItem): Boolean {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
