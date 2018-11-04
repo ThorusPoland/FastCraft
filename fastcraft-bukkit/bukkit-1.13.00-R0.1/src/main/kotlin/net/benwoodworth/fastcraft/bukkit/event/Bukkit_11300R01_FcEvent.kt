d5a2b41158018ac8b5c4819e3375493e3d670cbc
ben <ben@benwoodworth.net>
2018-11-03 23:14:24 -0400

    Implemented Bukkit platform events

---
+++ fastcraft-bukkit/bukkit-1.13.00-R0.1/src/main/kotlin/net/benwoodworth/fastcraft/bukkit/event/Bukkit_11300R01_FcEvent.kt
@@ -0,0 +1,8 @@
+package net.benwoodworth.fastcraft.bukkit.event
+
+import net.benwoodworth.fastcraft.platform.event.FcEvent
+import org.bukkit.event.Event
+
+inline class Bukkit_11300R01_FcEvent(
+    val bukkitEvent: Event
+) : FcEvent
