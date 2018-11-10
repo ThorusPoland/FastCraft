d3c6057deb684dde8c77e64dc57e36be140ae28a
BenWoodworth <ben@benwoodworth.net>
2018-11-10 13:44:06 -0500

    Remove inline classes.

--- fastcraft-bukkit/bukkit-1.13.00-R0.1/src/main/kotlin/net/benwoodworth/fastcraft/bukkit/event/Bukkit_11300R01_FcEvent.kt
+++ fastcraft-bukkit/bukkit-1.13.00-R0.1/src/main/kotlin/net/benwoodworth/fastcraft/bukkit/event/Bukkit_11300R01_FcEvent.kt
@@ -4,6 +4,6 @@ import net.benwoodworth.fastcraft.platform.event.FcEvent
 import org.bukkit.event.Event
 
 @Suppress("ClassName")
-inline class Bukkit_11300R01_FcEvent(
+class Bukkit_11300R01_FcEvent(
     val bukkitEvent: Event
 ) : FcEvent
