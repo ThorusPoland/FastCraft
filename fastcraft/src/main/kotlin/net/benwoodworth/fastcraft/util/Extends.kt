c26c29339e79f6be89ca3a8f9dfea70f22cdfb47
BenWoodworth <ben@benwoodworth.net>
2018-11-13 12:48:49 -0500

    Moved Extensible ode into the same file.

--- fastcraft/src/main/kotlin/net/benwoodworth/fastcraft/util/Extends.kt
+++
@@ -1,6 +0,0 @@
-package net.benwoodworth.fastcraft.util
-
-interface Extends<out T : Extensible> : Extensible {
-
-    val base: T
-}
--- fastcraft/src/main/kotlin/net/benwoodworth/fastcraft/util/Extensible.kt
+++ fastcraft/src/main/kotlin/net/benwoodworth/fastcraft/util/Extensible.kt
@@ -1,3 +1,21 @@
 package net.benwoodworth.fastcraft.util
 
 interface Extensible
+
+interface Extends<out T : Extensible> : Extensible {
+
+    val base: T
+}
+
+inline fun <reified T> Extensible.getAs(): T {
+    var result = this
+
+    while (result is Extends<*>) {
+        when (result) {
+            is T -> return result
+            else -> result = result.base
+        }
+    }
+
+    return result as T
+}
