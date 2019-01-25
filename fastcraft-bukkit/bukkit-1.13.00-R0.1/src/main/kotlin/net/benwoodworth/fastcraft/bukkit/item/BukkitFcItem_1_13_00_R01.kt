c436b5e9bab39edfac7ec5929e19e2df85943a40
BenWoodworth <ben@benwoodworth.net>
2019-01-25 16:35:30 -0500

    Remove toItemStack() from BukkitFcItem_1_13_00_R01

--- fastcraft-bukkit/bukkit-1.13.00-R0.1/src/main/kotlin/net/benwoodworth/fastcraft/bukkit/item/BukkitFcItem_1_13_00_R01.kt
+++ fastcraft-bukkit/bukkit-1.13.00-R0.1/src/main/kotlin/net/benwoodworth/fastcraft/bukkit/item/BukkitFcItem_1_13_00_R01.kt
@@ -1,8 +1,6 @@
 package net.benwoodworth.fastcraft.bukkit.item
 
-import net.benwoodworth.fastcraft.platform.item.FcItem
 import net.benwoodworth.fastcraft.platform.item.FcItemType
-import net.benwoodworth.fastcraft.platform.locale.FcLocale
 import net.benwoodworth.fastcraft.platform.text.FcText
 import org.bukkit.inventory.ItemStack
 
@@ -11,10 +9,6 @@ class BukkitFcItem_1_13_00_R01(
     override val displayName: FcText?,
     override val lore: List<FcText>?
 ) : BukkitFcItem {
-
-    override fun toItemStack(item: FcItem, locale: FcLocale): ItemStack {
-        TODO("not implemented")
-    }
 
     override val type: FcItemType
         get() = TODO("not implemented")
