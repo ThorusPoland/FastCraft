49036e983693e0a1bc02205f355b3dcf437e9852
BenWoodworth <ben@benwoodworth.net>
2019-01-25 17:19:55 -0500

    Fix .toRaw() error in BukkitFcPlayer_1_13_00_R01.

--- fastcraft-bukkit/bukkit-1.13.00-R0.1/src/main/kotlin/net/benwoodworth/fastcraft/bukkit/player/BukkitFcPlayerProvider_1_13_00_R01.kt
+++ fastcraft-bukkit/bukkit-1.13.00-R0.1/src/main/kotlin/net/benwoodworth/fastcraft/bukkit/player/BukkitFcPlayerProvider_1_13_00_R01.kt
@@ -1,20 +1,24 @@
 package net.benwoodworth.fastcraft.bukkit.player
 
+import net.benwoodworth.fastcraft.bukkit.text.BukkitFcTextConverter
 import net.benwoodworth.fastcraft.platform.player.FcPlayer
 import org.bukkit.Bukkit
 import java.util.*
+import javax.inject.Inject
 
-class BukkitFcPlayerProvider_1_13_00_R01 : BukkitFcPlayerProvider {
+class BukkitFcPlayerProvider_1_13_00_R01 @Inject constructor(
+    private val textConverter: BukkitFcTextConverter
+) : BukkitFcPlayerProvider {
 
     override fun getOnlinePlayers(): List<FcPlayer> {
         return Bukkit.getOnlinePlayers().map { player ->
-            BukkitFcPlayer_1_13_00_R01(player)
+            BukkitFcPlayer_1_13_00_R01(player, textConverter)
         }
     }
 
     override fun getPlayer(uuid: UUID): FcPlayer? {
         return Bukkit.getPlayer(uuid)?.let { player ->
-            BukkitFcPlayer_1_13_00_R01(player)
+            BukkitFcPlayer_1_13_00_R01(player, textConverter)
         }
     }
 }
--- fastcraft-bukkit/bukkit-1.13.00-R0.1/src/main/kotlin/net/benwoodworth/fastcraft/bukkit/player/BukkitFcPlayer_1_13_00_R01.kt
+++ fastcraft-bukkit/bukkit-1.13.00-R0.1/src/main/kotlin/net/benwoodworth/fastcraft/bukkit/player/BukkitFcPlayer_1_13_00_R01.kt
@@ -1,14 +1,15 @@
 package net.benwoodworth.fastcraft.bukkit.player
 
-import net.benwoodworth.fastcraft.bukkit.bukkit
 import net.benwoodworth.fastcraft.bukkit.locale.BukkitFcLocale_1_13_00_R01
+import net.benwoodworth.fastcraft.bukkit.text.BukkitFcTextConverter
 import net.benwoodworth.fastcraft.platform.locale.FcLocale
 import net.benwoodworth.fastcraft.platform.text.FcText
 import org.bukkit.entity.Player
 import java.util.*
 
 class BukkitFcPlayer_1_13_00_R01(
-    override val player: Player
+    override val player: Player,
+    private val textConverter: BukkitFcTextConverter
 ) : BukkitFcPlayer {
 
     override val username: String
@@ -27,7 +28,9 @@ class BukkitFcPlayer_1_13_00_R01(
         get() = BukkitFcLocale_1_13_00_R01(player.locale)
 
     override fun sendMessage(message: FcText) {
-        player.sendRawMessage(message.bukkit.toRaw())
+        with(textConverter) {
+            player.sendRawMessage(message.toRaw())
+        }
     }
 
     override fun hasPermission(permission: String): Boolean {
