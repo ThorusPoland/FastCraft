72815002c2545d63690efc24bf092f488d9038cf
BenWoodworth <ben@benwoodworth.net>
2019-01-26 00:30:00 -0500

    Add default implementations for BukkitFcGuiLayoutSlot.

--- fastcraft-bukkit/bukkit/src/main/kotlin/net/benwoodworth/fastcraft/bukkit/gui/BukkitFcGuiLayoutSlot.kt
+++ fastcraft-bukkit/bukkit/src/main/kotlin/net/benwoodworth/fastcraft/bukkit/gui/BukkitFcGuiLayoutSlot.kt
@@ -2,4 +2,19 @@ package net.benwoodworth.fastcraft.bukkit.gui
 
 import net.benwoodworth.fastcraft.platform.gui.FcGuiLayoutSlot
 
-interface BukkitFcGuiLayoutSlot : FcGuiLayoutSlot
+interface BukkitFcGuiLayoutSlot : FcGuiLayoutSlot {
+
+    override val width: Int
+        get() = 1
+
+    override val height: Int
+        get() = 1
+
+    override fun get(column: Int, row: Int): FcGuiLayoutSlot {
+        if (column == 0 && row == 0) {
+            return this
+        } else {
+            throw IllegalArgumentException()
+        }
+    }
+}
