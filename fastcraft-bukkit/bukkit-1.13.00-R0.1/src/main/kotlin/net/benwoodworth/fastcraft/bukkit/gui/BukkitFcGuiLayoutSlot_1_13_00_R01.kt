82c11ef73582b4358d0f7ec46fd9a10d325fee6b
BenWoodworth <ben@benwoodworth.net>
2019-01-26 00:30:20 -0500

    Implement BukkitFcGuiLayoutSlot_1_13_00_R01.item.

--- fastcraft-bukkit/bukkit-1.13.00-R0.1/src/main/kotlin/net/benwoodworth/fastcraft/bukkit/gui/BukkitFcGuiLayoutSlot_1_13_00_R01.kt
+++ fastcraft-bukkit/bukkit-1.13.00-R0.1/src/main/kotlin/net/benwoodworth/fastcraft/bukkit/gui/BukkitFcGuiLayoutSlot_1_13_00_R01.kt
@@ -1,24 +1,24 @@
 package net.benwoodworth.fastcraft.bukkit.gui
 
+import net.benwoodworth.fastcraft.bukkit.bukkit
 import net.benwoodworth.fastcraft.platform.gui.FcGuiClickEvent
-import net.benwoodworth.fastcraft.platform.gui.FcGuiLayoutSlot
 import net.benwoodworth.fastcraft.platform.item.FcItem
+import net.benwoodworth.fastcraft.platform.item.FcItemFactory
+import org.bukkit.inventory.Inventory
 
-class BukkitFcGuiLayoutSlot_1_13_00_R01 : BukkitFcGuiLayoutSlot {
-
-    override val width: Int
-        get() = TODO("not implemented")
-
-    override val height: Int
-        get() = TODO("not implemented")
-
-    override fun get(column: Int, row: Int): FcGuiLayoutSlot {
-        TODO("not implemented")
-    }
+class BukkitFcGuiLayoutSlot_1_13_00_R01(
+    private val inventory: Inventory,
+    private val slotIndex: Int,
+    private val itemFactory: FcItemFactory
+) : BukkitFcGuiLayoutSlot {
 
     override var item: FcItem?
-        get() = TODO("not implemented")
-        set(value) {}
+        get() = with(itemFactory.bukkit) {
+            FcItem(inventory.getItem(slotIndex))
+        }
+        set(value) {
+            inventory.setItem(slotIndex, value?.bukkit?.itemStack?.clone())
+        }
 
     override fun setClickHandler(column: Int, row: Int, handler: (event: FcGuiClickEvent) -> Unit) {
         TODO("not implemented")
