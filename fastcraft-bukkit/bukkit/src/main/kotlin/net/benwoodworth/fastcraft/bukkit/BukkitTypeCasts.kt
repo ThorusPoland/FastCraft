2f4a86a65b2463f776a288a15a3698088479f533
BenWoodworth <ben@benwoodworth.net>
2019-01-25 23:50:25 -0500

    Fix BukkitTypeCasts.

--- fastcraft-bukkit/bukkit/src/main/kotlin/net/benwoodworth/fastcraft/bukkit/BukkitTypeCasts.kt
+++ fastcraft-bukkit/bukkit/src/main/kotlin/net/benwoodworth/fastcraft/bukkit/BukkitTypeCasts.kt
@@ -4,11 +4,7 @@ import net.benwoodworth.fastcraft.bukkit.config.BukkitFcConfig
 import net.benwoodworth.fastcraft.bukkit.config.BukkitFcConfigEntry
 import net.benwoodworth.fastcraft.bukkit.config.BukkitFcConfigFactory
 import net.benwoodworth.fastcraft.bukkit.event.BukkitFcEvent
-import net.benwoodworth.fastcraft.bukkit.gui.BukkitFcGui
-import net.benwoodworth.fastcraft.bukkit.gui.BukkitFcGuiFactory
-import net.benwoodworth.fastcraft.bukkit.gui.types.BukkitFcGuiChest
-import net.benwoodworth.fastcraft.bukkit.gui.types.BukkitFcGuiDispenser
-import net.benwoodworth.fastcraft.bukkit.gui.types.BukkitFcGuiHopper
+import net.benwoodworth.fastcraft.bukkit.gui.*
 import net.benwoodworth.fastcraft.bukkit.item.BukkitFcItem
 import net.benwoodworth.fastcraft.bukkit.item.BukkitFcItemFactory
 import net.benwoodworth.fastcraft.bukkit.item.BukkitFcItemType
@@ -32,11 +28,7 @@ import net.benwoodworth.fastcraft.platform.config.FcConfig
 import net.benwoodworth.fastcraft.platform.config.FcConfigEntry
 import net.benwoodworth.fastcraft.platform.config.FcConfigFactory
 import net.benwoodworth.fastcraft.platform.event.FcEvent
-import net.benwoodworth.fastcraft.platform.gui.FcGui
-import net.benwoodworth.fastcraft.platform.gui.FcGuiFactory
-import net.benwoodworth.fastcraft.platform.gui.types.FcGuiChest
-import net.benwoodworth.fastcraft.platform.gui.types.FcGuiDispenser
-import net.benwoodworth.fastcraft.platform.gui.types.FcGuiHopper
+import net.benwoodworth.fastcraft.platform.gui.*
 import net.benwoodworth.fastcraft.platform.item.FcItem
 import net.benwoodworth.fastcraft.platform.item.FcItemFactory
 import net.benwoodworth.fastcraft.platform.item.FcItemType
@@ -75,9 +67,6 @@ inline val FcTextColor.bukkit: BukkitFcTextColor
 inline val FcText.bukkit: BukkitFcText
     get() = this as BukkitFcText
 
-//inline val FcServer.bukkit: BukkitFcServer
-//    get() = this as BukkitFcServer
-
 inline val FcPluginData.bukkit: BukkitFcPluginData
     get() = this as BukkitFcPluginData
 
@@ -95,9 +84,6 @@ inline val FcCraftingRecipe.bukkit: BukkitFcCraftingRecipe
 
 inline val FcPlayerProvider.bukkit: BukkitFcPlayerProvider
     get() = this as BukkitFcPlayerProvider
-
-//inline val FcPlayerJoinEvent.bukkit: BukkitFcPlayerJoinEvent
-//    get() = this as BukkitFcPlayerJoinEvent
 
 inline val FcPlayer.bukkit: BukkitFcPlayer
     get() = this as BukkitFcPlayer
@@ -120,35 +106,26 @@ inline val FcItemFactory.bukkit: BukkitFcItemFactory
 inline val FcItem.bukkit: BukkitFcItem
     get() = this as BukkitFcItem
 
-//inline val FcGuiCloseEvent.bukkit: BukkitFcGuiCloseEvent
-//    get() = this as BukkitFcGuiCloseEvent
+inline val <T : FcGuiLayout> FcGui<T>.bukkit: BukkitFcGui<T>
+    get() = this as BukkitFcGui<T>
 
-//inline val FcGuiClickEvent.bukkit: BukkitFcGuiClickEvent
-//    get() = this as BukkitFcGuiClickEvent
+inline val FcGuiClickEvent.bukkit: BukkitFcGuiClickEvent
+    get() = this as BukkitFcGuiClickEvent
+
+inline val FcGuiCloseEvent.bukkit: BukkitFcGuiCloseEvent
+    get() = this as BukkitFcGuiCloseEvent
 
 inline val FcGuiFactory.bukkit: BukkitFcGuiFactory
     get() = this as BukkitFcGuiFactory
 
-inline val FcGui.bukkit: BukkitFcGui
-    get() = this as BukkitFcGui
+inline val FcGuiLayout.bukkit: BukkitFcGuiLayout
+    get() = this as BukkitFcGuiLayout
 
-inline val FcGuiHopper.bukkit: BukkitFcGuiHopper
-    get() = this as BukkitFcGuiHopper
+inline val FcGuiLayoutGrid.bukkit: BukkitFcGuiLayoutGrid
+    get() = this as BukkitFcGuiLayoutGrid
 
-//inline val FcGuiGrid.bukkit: BukkitFcGuiGrid
-//    get() = this as BukkitFcGuiGrid
-
-inline val FcGuiDispenser.bukkit: BukkitFcGuiDispenser
-    get() = this as BukkitFcGuiDispenser
-
-inline val FcGuiChest.bukkit: BukkitFcGuiChest
-    get() = this as BukkitFcGuiChest
-
-//inline val FcEventListener.bukkit: BukkitFcEventListener
-//    get() = this as BukkitFcEventListener
-
-//inline val FcEventHandler.bukkit: BukkitFcEventHandler
-//    get() = this as BukkitFcEventHandler
+inline val FcGuiLayoutSlot.bukkit: BukkitFcGuiLayoutSlot
+    get() = this as BukkitFcGuiLayoutSlot
 
 inline val FcEvent.bukkit: BukkitFcEvent
     get() = this as BukkitFcEvent
@@ -161,6 +138,3 @@ inline val FcConfigEntry.bukkit: BukkitFcConfigEntry
 
 inline val FcConfig.bukkit: BukkitFcConfig
     get() = this as BukkitFcConfig
-
-//inline val FcPlatform.bukkit: BukkitFcPlatformDependencies
-//    get() = this as BukkitFcPlatformDependencies
