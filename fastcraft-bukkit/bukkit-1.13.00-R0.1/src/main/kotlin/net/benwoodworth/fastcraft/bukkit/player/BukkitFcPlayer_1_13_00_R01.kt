002503d572dd5b4e4a87502533da69809fb54109
BenWoodworth <ben@benwoodworth.net>
2019-01-25 22:39:08 -0500

    Store UUID instead of Player in BukkitFcPlayer_1_13_00_R01.

--- fastcraft-bukkit/bukkit-1.13.00-R0.1/src/main/kotlin/net/benwoodworth/fastcraft/bukkit/player/BukkitFcPlayer_1_13_00_R01.kt
+++ fastcraft-bukkit/bukkit-1.13.00-R0.1/src/main/kotlin/net/benwoodworth/fastcraft/bukkit/player/BukkitFcPlayer_1_13_00_R01.kt
@@ -4,13 +4,16 @@ import net.benwoodworth.fastcraft.bukkit.locale.BukkitFcLocale_1_13_00_R01
 import net.benwoodworth.fastcraft.bukkit.text.BukkitFcTextConverter
 import net.benwoodworth.fastcraft.platform.locale.FcLocale
 import net.benwoodworth.fastcraft.platform.text.FcText
+import org.bukkit.Bukkit
 import org.bukkit.entity.Player
 import java.util.*
 
 class BukkitFcPlayer_1_13_00_R01(
-    override val player: Player,
+    override val uuid: UUID,
     private val textConverter: BukkitFcTextConverter
 ) : BukkitFcPlayer {
+
+    override val player: Player = Bukkit.getPlayer(uuid)
 
     override val username: String
         get() = player.name
@@ -20,9 +23,6 @@ class BukkitFcPlayer_1_13_00_R01(
         set(value) {
             player.customName = value
         }
-
-    override val uuid: UUID
-        get() = player.uniqueId
 
     override val locale: FcLocale
         get() = BukkitFcLocale_1_13_00_R01(player.locale)
