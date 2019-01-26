f127ab25cb6b527735a5af948a630dad321bf033
BenWoodworth <ben@benwoodworth.net>
2019-01-25 21:06:43 -0500

    Add test-server/ directory for test servers, with FastCraft install script.

---
+++ test-servers/copy-fastcraft.sh
@@ -0,0 +1,18 @@
+#! /bin/sh
+# Copies "build/libs/FastCraft*.jar" to the test servers.
+
+# Bukkit Servers
+for d in test-servers/bukkit/*/
+do
+    mkdir -p "$d"/plugins/update/
+    rm -f "$d"/plugins/update/FastCraft*.jar
+    cp build/libs/FastCraft*.jar "$d"/plugins/update/FastCraft.jar
+done
+
+# Sponge Servers
+for d in "test-servers/sponge/*/"
+do
+    mkdir -p "$d"/mods/plugins/
+    rm -f "$d"/mods/plugins/update/FastCraft*.jar
+    cp build/libs/FastCraft*.jar "$d"/mods/plugins/FastCraft.jar
+done
