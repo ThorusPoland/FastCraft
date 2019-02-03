630daa0a72cdd8129309c264ad2c50d13cc1d50c
BenWoodworth <ben@benwoodworth.net>
2019-02-03 14:11:08 -0500

    Implement BukkitFcGuiClickEvent_1_13_00_R01.

--- fastcraft-bukkit/bukkit-1.13.00-R0.1/src/main/kotlin/net/benwoodworth/fastcraft/bukkit/gui/BukkitFcGuiClickEvent_1_13_00_R01.kt
+++ fastcraft-bukkit/bukkit-1.13.00-R0.1/src/main/kotlin/net/benwoodworth/fastcraft/bukkit/gui/BukkitFcGuiClickEvent_1_13_00_R01.kt
@@ -3,21 +3,13 @@ package net.benwoodworth.fastcraft.bukkit.gui
 import net.benwoodworth.fastcraft.platform.gui.FcGui
 import net.benwoodworth.fastcraft.platform.player.FcPlayer
 
-class BukkitFcGuiClickEvent_1_13_00_R01 : BukkitFcGuiClickEvent {
-    override val gui: FcGui<*>
-        get() = TODO("not implemented")
-    override val player: FcPlayer
-        get() = TODO("not implemented")
-    override val isPrimaryClick: Boolean
-        get() = TODO("not implemented")
-    override val isSecondaryClick: Boolean
-        get() = TODO("not implemented")
-    override val isMiddleClick: Boolean
-        get() = TODO("not implemented")
-    override val isDoubleClick: Boolean
-        get() = TODO("not implemented")
-    override val isShiftClick: Boolean
-        get() = TODO("not implemented")
+class BukkitFcGuiClickEvent_1_13_00_R01(
+    override val gui: FcGui<*>,
+    override val player: FcPlayer,
+    override val isPrimaryClick: Boolean,
+    override val isSecondaryClick: Boolean,
+    override val isMiddleClick: Boolean,
+    override val isDoubleClick: Boolean,
+    override val isShiftClick: Boolean,
     override val numberKeyPressed: Int?
-        get() = TODO("not implemented")
-}
+) : BukkitFcGuiClickEvent
