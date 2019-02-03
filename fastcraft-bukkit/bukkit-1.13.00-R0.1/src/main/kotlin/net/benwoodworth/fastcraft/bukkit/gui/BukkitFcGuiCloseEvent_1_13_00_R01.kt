a63e49e77db99c4d17d6032887b665d69cc5ee3a
BenWoodworth <ben@benwoodworth.net>
2019-02-03 14:11:51 -0500

    Implement BukkitFcGuiCloseEvent_1_13_00_R01.

--- fastcraft-bukkit/bukkit-1.13.00-R0.1/src/main/kotlin/net/benwoodworth/fastcraft/bukkit/gui/BukkitFcGuiCloseEvent_1_13_00_R01.kt
+++ fastcraft-bukkit/bukkit-1.13.00-R0.1/src/main/kotlin/net/benwoodworth/fastcraft/bukkit/gui/BukkitFcGuiCloseEvent_1_13_00_R01.kt
@@ -3,9 +3,7 @@ package net.benwoodworth.fastcraft.bukkit.gui
 import net.benwoodworth.fastcraft.platform.gui.FcGui
 import net.benwoodworth.fastcraft.platform.player.FcPlayer
 
-class BukkitFcGuiCloseEvent_1_13_00_R01 : BukkitFcGuiCloseEvent {
-    override val gui: FcGui<*>
-        get() = TODO("not implemented")
+class BukkitFcGuiCloseEvent_1_13_00_R01(
+    override val gui: FcGui<*>,
     override val player: FcPlayer
-        get() = TODO("not implemented")
-}
+) : BukkitFcGuiCloseEvent
